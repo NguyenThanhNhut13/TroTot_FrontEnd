6153538adc27d52685ea2f6a03c550a9ff08db7b
letanphat0107 <phat172003@gmail.com>
2025-04-15 21:26:07 +0700

    fix login

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// index.tsx hoặc main.tsx
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
+const root = ReactDOM.createRoot(document.getElementById('root')!)
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
-);
+)
