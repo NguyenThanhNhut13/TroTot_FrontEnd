54085dd2b07a6e27f6c3e90d03d80d5ab68e7584
NGÔ VĂN TOÀN <ngovantoan0986@gmail.com>
2025-05-18 17:00:06 +0700

    render review page

--- src/apis/payment.api.ts
+++ src/apis/payment.api.ts
@@ -1,25 +1,20 @@
-import { add, get } from "lodash";
 import http from "../utils/http";
 import { SuccessResponse } from "../types/utils.type";
-import exp from "constants";
-import userApi from './user.api';
-import { PaypalPayment } from "../types/paypal.type";
 
-export const URL_PURCHASE_SLOT = "api/v1/users/use-post-slot"
-export const URL_GET_WALLET = "api/v1/payments/wallet"
-export const URL_THEMTIEN_WALLET = "api/v1/payments/vn-pay"
+export const URL_ADD_POST_SLOTS = "api/v1/users/add-post-slots"; // Match backend endpoint
+export const URL_GET_WALLET = "api/v1/payments/wallet";
+export const URL_THEMTIEN_WALLET = "api/v1/payments/vn-pay";
 
 const paymentAPI = {
-    purchaseSlot(body: { amount: number}) {
-        return http.post<SuccessResponse<any>>(URL_PURCHASE_SLOT, body)
-    },
-    getWallet(userId: number) {
-        return http.get<SuccessResponse<any>>(`${URL_GET_WALLET}/${userId}`)
-    },
-    addMoneyToWallet(userId: number, amount: number) {
-        return http.get<SuccessResponse<PaypalPayment>>(`${URL_THEMTIEN_WALLET}?amount=${amount}&userId=${userId}&bankCode=NCB`)
-    }
-
-}
+  purchaseSlot(body: { amount: number }) {
+    return http.post<SuccessResponse<any>>(URL_ADD_POST_SLOTS, body);
+  },
+  getWallet(userId: number) {
+    return http.get<SuccessResponse<any>>(`${URL_GET_WALLET}/${userId}`);
+  },
+  addMoneyToWallet(userId: number, amount: number) {
+    return http.get<SuccessResponse<any>>(`${URL_THEMTIEN_WALLET}?amount=${amount}&userId=${userId}&bankCode=NCB`);
+  },
+};
 
 export default paymentAPI;
