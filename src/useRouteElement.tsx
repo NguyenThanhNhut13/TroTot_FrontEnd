dd9c66336ee64c7883a15cac89aad45cc27b3bb8
NGÔ VĂN TOÀN <ngovantoan0986@gmail.com>
2025-05-19 16:20:09 +0700

    history payment

--- src/useRouteElement.tsx
+++ src/useRouteElement.tsx
@@ -13,6 +13,7 @@ import SavedRoom from "./pages/Profile/SavedRoom";
 import Notifications from "./pages/Profile/Notifycations";
 import ReviewRoom from "./pages/Profile/ReviewRoom";
 import DetailRoom from "./pages/Navigation/DetailRoom";
+import HistoryPage from "./pages/PayPage/HistoryPage";
 
 export default function useRouteElement() {
   const routeElement = useRoutes([
@@ -68,7 +69,11 @@ export default function useRouteElement() {
         {
           path: "/phong-tro/:id",
           element: <DetailRoom />, // thêm đường dẫn mới cho trang chi tiết phòng
-        }
+        },
+        {
+          path: "/history",
+          element: <HistoryPage />, // Lịch sử giao dịch
+        },
       ],
     },
   ]);
